ecf2ad8556e1e3468e3bae74afa3e0c78aa28145
Brandon Kindred <brandon.kindred@gmail.com>
2018-01-04 11:39:55 -0500

    Removed ability to add groups that already are associated with a test

--- app/components/discovery/controllers.js
+++ app/components/discovery/controllers.js
@@ -158,6 +158,15 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.WorkAllocationService
          $scope.errors.push("Group name cannot be empty");
          return;
       }
+
+      //Check if group already exists before creating adding it
+      for(var i=0; i < test.groups.length; i++){
+          if(test.groups[i].name === group.name){
+            $scope.showExistingGroupNotice = true;
+            return;
+          }
+      }
+      
       Tester.addGroup({name: group.name,
                        description: group.description,
                        key: test.key}).$promise
