3371554336897df55ad48e4c3647cd220f3bdd7b
Brandon Kindred <brandon.kindred@gmail.com>
2018-03-13 13:11:33 -0400

    mostly code cleaning

--- app/components/main/controller.js
+++ app/components/main/controller.js
@@ -66,7 +66,6 @@ angular.module('Qanairy.main', ['ui.router'])
 
     $scope.$on('domain_updated', function(){
       $scope.domain = store.get('domain');
-      $scope.$apply();
     })
 
     $scope.$on('updateFailingCnt', function(){
