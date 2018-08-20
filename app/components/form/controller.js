2f06ddb459b47e31d99340ad7cb4ca6682d7bb5e
Brandon Kindred <brandon.kindred@gmail.com>
2018-08-20 19:03:13 -0400

    started creating form index, edit and added subscription to pusher channel for discovered tests

--- app/components/form/controller.js
+++ app/components/form/controller.js
@@ -12,6 +12,24 @@ angular.module('Qanairy.form', ['ui.router', 'Qanairy.FormService'])
 
 .controller('FormCtrl', ['$rootScope', '$scope', 'Form', 'store', '$state',
   function($rootScope, $scope, Form, store, $state) {
+    var pusher = new Pusher('77fec1184d841b55919e', {
+      cluster: 'us2',
+      encrypted: true
+    });
 
+    var channel = pusher.subscribe($scope.extractHostname($scope.domain_url));
+    channel.bind('discovered-form', function(data) {
+      var reported_form = JSON.parse(data);
+      console.log("reported form :: "+reported_form);
+      for(var idx=0; idx<$scope.tests.length; idx++){
+        var test = $scope.tests[idx];
+        if(test.key == reported_test.key){
+          $scope.tests[idx] = reported_test;
+          break;
+        }
+      }
+      $scope.waitingOnTests = false;
+      $scope.$apply();
+    });
   }
 ]);
