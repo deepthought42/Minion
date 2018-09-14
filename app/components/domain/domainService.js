f6841f2ad53772217e69ddbe749b3ea50e182d66
Brandon Kindred <brandon.kindred@gmail.com>
2018-09-13 20:56:47 -0400

    made saving users work

--- app/components/domain/domainService.js
+++ app/components/domain/domainService.js
@@ -3,9 +3,10 @@
 var domainService = angular.module('Qanairy.DomainService', ['ngResource', 'Qanairy.serviceConfig']);
 
 domainService.factory('Domain', ['$resource', 'Qanairy.serviceConfig', function ($resource, config) {
-  return $resource(config.basePath + '/domains', {id: '@id'}, {
+  return $resource(config.basePath + '/domains', {domain_id: '@domain_id'}, {
     update: { method: 'PUT', params: {key: '@key', protocol: '@protocol', browser_name: '@browser_name', logoUrl: '@logoUrl'}},
     save: { method: 'POST', params: {protocol: '@protocol', browser_name: '@browser_name', url: '@url', logoUrl: '@logoUrl'}},
+    saveUser:{url: config.basePath + '/domains/:domain_id/users', method: 'POST', params: {username: '@username', password: '@password', role: '@role', enabled: '@enabled'}},
     getForms: {url: config.basePath + '/domains/:key/forms', method: 'GET', isArray: true, params:{key: '@key'}},
     getAllPageStates: {url: config.basePath + '/domains/page_states', method: 'GET', isArray: true, params: {host: '@host'}},
     getAllPageElements: {url: config.basePath + '/domains/page_elements', method: 'GET', isArray: true, params: {host: '@host'}},
--- app/components/user/edit_controller.js
+++ app/components/user/edit_controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('Qanairy.user_edit', ['ui.router', 'Qanairy.TestUserService'])
+angular.module('Qanairy.user_edit', ['ui.router', 'Qanairy.TestUserService', 'Qanairy.DomainService'])
 
 .config(['$stateProvider', function($stateProvider) {
   $stateProvider.state('main.edit_user', {
@@ -10,8 +10,23 @@ angular.module('Qanairy.user_edit', ['ui.router', 'Qanairy.TestUserService'])
   });
 }])
 
-.controller('UserEditCtrl', ['$rootScope', '$scope', 'TestUser', 'store', '$state',
-  function($rootScope, $scope, TestUser, store, $state) {
+.controller('UserEditCtrl', ['$rootScope', '$scope', 'TestUser', 'Domain', 'store', '$state',
+  function($rootScope, $scope, TestUser, Domain, store, $state) {
 
+    this._init = function(){
+      $scope.domain = store.get('domain');
+    }
+
+    $scope.create_user = function(user){
+      console.log(user);
+      console.log($scope.domain);
+      Domain.saveUser({domain_id: $scope.domain.id, username: user.username, password: user.password, role: user.role, enabled: user.isEnabled}).$promise
+        .then(function(){
+          console.log("user saved successfully");
+          $state.go("main.users")
+        });
+    }
+
+    this._init();
   }
 ]);
