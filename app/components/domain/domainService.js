06aba54e507b1f0219ab35aef4dc222a7d096a07
Brandon Kindred <brandon.kindred@gmail.com>
2018-09-17 20:45:11 -0400

    added in endpoints and fuctionality for managing users and forms

--- app/components/domain/domainService.js
+++ app/components/domain/domainService.js
@@ -7,9 +7,9 @@ domainService.factory('Domain', ['$resource', 'Qanairy.serviceConfig', function 
     update: { method: 'PUT', params: {key: '@key', protocol: '@protocol', browser_name: '@browser_name', logoUrl: '@logoUrl'}},
     save: { method: 'POST', params: {protocol: '@protocol', browser_name: '@browser_name', url: '@url', logoUrl: '@logoUrl'}},
     saveUser:{url: config.basePath + '/domains/:domain_id/users', method: 'POST', params: {username: '@username', password: '@password', role: '@role', enabled: '@enabled'}},
-    getUsers: {url: config.basePath + '/domains/:domain_id/users', method: 'GET', isArray: true},
-    getForms: {url: config.basePath + '/domains/:key/forms', method: 'GET', isArray: true, params:{key: '@key'}},
-    updateForm: {url: config.basePath + '/domains/:key/forms', method: 'PUT', params: {key: '@key', form: '@form'}},
+    getUsers: {url: config.basePath + '/domains/:domain_id/users', method: 'GET',  params: {key: '@key'}, isArray: true},
+    getForms: {url: config.basePath + '/domains/:key/forms', method: 'GET', isArray: true, params:{key: '@key', type: '@type'}},
+    updateForm: {url: config.basePath + '/domains/:domain_id/forms', method: 'PUT', params: {key: '@key', type: '@form_type'}},
     getAllPageStates: {url: config.basePath + '/domains/page_states', method: 'GET', isArray: true, params: {host: '@host'}},
     getAllPageElements: {url: config.basePath + '/domains/page_elements', method: 'GET', isArray: true, params: {host: '@host'}},
     getAllPathObjects: {url: config.basePath + '/domains/path', method: 'GET', isArray: true, params: {host: '@host'}}
--- app/components/form/edit_controller.js
+++ app/components/form/edit_controller.js
@@ -15,7 +15,7 @@ angular.module('Qanairy.form_edit', ['ui.router', 'Qanairy.FormService', 'Qanair
   function($rootScope, $scope, Form, Domain, store, $state, $stateParams) {
 
     this._init = function(){
-      $scope.domain = state.get('domain');
+      $scope.domain = store.get('domain');
       if($stateParams.form){
         $scope.form = $stateParams.form;
         store.set('current_form', $scope.form);
@@ -27,8 +27,8 @@ angular.module('Qanairy.form_edit', ['ui.router', 'Qanairy.FormService', 'Qanair
     };
 
     $scope.discoverTests = function(form){
-      Domain.updateForm($scope.domain.id, form).$promise
-        then(function(data){
+      Domain.updateForm({domain_id: $scope.domain.id, key: form.key, form_type: form.type}).$promise
+        .then(function(data){
           console.log("Successfully updated form");
         })
         .catch(function(err){
@@ -36,44 +36,6 @@ angular.module('Qanairy.form_edit', ['ui.router', 'Qanairy.FormService', 'Qanair
         })
     }
 
-    $scope.updateField = function(){
-      if(required_rule){
-        //create required rule
-
-      }
-
-      if(min_max_characters_rules){
-          //create min max rules
-      }
-
-      if(min_max_value_rules){
-          //
-      }
-
-      if(special_character_restriction){
-          //
-      }
-
-      if(alphabetic_character_restriction){
-          //
-      }
-
-      if(numeric_restriction){
-          //
-      }
-
-      if(whitespace_restriction){
-          //
-      }
-
-      if(case_sensitive){
-          //
-      }
-
-      if(email_pattern_rule){
-          //
-      }
-    }
     this._init();
   }
 ]);
