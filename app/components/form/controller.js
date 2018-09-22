49eea3ca1eba20c02d2d5f11f664a8ad2dfe709f
Brandon Kindred <brandon.kindred@gmail.com>
2018-09-22 19:32:54 -0400

    fixed form real time reporting

--- app/components/form/controller.js
+++ app/components/form/controller.js
@@ -36,14 +36,14 @@ angular.module('Qanairy.form', ['ui.router', 'Qanairy.FormService', 'Qanairy.Dom
     channel.bind('discovered-form', function(data) {
       var reported_form = JSON.parse(data);
       console.log("reported form :: "+reported_form);
-      for(var idx=0; idx<$scope.tests.length; idx++){
-        var test = $scope.tests[idx];
-        if(test.key == reported_test.key){
-          $scope.tests[idx] = reported_test;
+      for(var idx=0; idx<$scope.forms.length; idx++){
+        var form = $scope.forms[idx];
+        if(form.key == reported_form.key){
+          $scope.forms[idx] = reported_form;
           break;
         }
       }
-      $scope.waitingOnTests = false;
+      $scope.waitingOnForms = false;
       $scope.$apply();
     });
 
