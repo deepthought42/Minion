da215c06e1b8a3e71f7c5c81ab71dca5f6529e97
Brandon Kindred <brandon.kindred@gmail.com>
2018-11-01 22:19:12 -0400

    subscribe service rename

--- app/components/services/subscribeService.js
+++ app/components/services/subscribeService.js
@@ -2,6 +2,7 @@
 
 var subscribeService = angular.module('Qanairy.SubscribeService', ['ngResource', 'Qanairy.serviceConfig']);
 
+subscribeService.factory('Subscribe', ['$resource', 'Qanairy.serviceConfig', function ($resource, config) {
   return $resource(config.basePath + '/subscribe', {id: '@id'}, {
     update: { method: 'PUT', params: {plan: '@plan'}}
   });
