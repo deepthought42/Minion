82ee0d5a178dbc0537a238909c53770036c38d52
Brandon Kindred <brandon.kindred@gmail.com>
2018-12-10 20:10:20 -0500

    logo wasn't showing up because of inproper parameter name.

--- app/components/domain/domainService.js
+++ app/components/domain/domainService.js
@@ -5,8 +5,8 @@ var domainService = angular.module('Qanairy.DomainService', ['ngResource', 'Qana
 domainService.factory('Domain', ['$resource', 'Qanairy.serviceConfig', function ($resource, config) {
   return $resource(config.basePath + '/domains', {domain_id: '@domain_id'}, {
     delete: { url: config.basePath + '/domains/:domain_id', method: 'DELETE'},
-    update: { method: 'PUT', params: {key: '@key', protocol: '@protocol', browser_name: '@browser_name', logoUrl: '@logoUrl'}},
-    save: { method: 'POST', params: {protocol: '@protocol', browser_name: '@browser_name', url: '@url', logoUrl: '@logoUrl'}},
+    update: { method: 'PUT', params: {key: '@key', protocol: '@protocol', browser_name: '@browser_name', logo_url: '@logoUrl'}},
+    save: { method: 'POST', params: {protocol: '@protocol', browser_name: '@browser_name', url: '@url', logo_url: '@logoUrl'}},
     addUser:{url: config.basePath + '/domains/:domain_id/users', method: 'POST', params: {username: '@username', password: '@password', role: '@role', enabled: '@enabled'}},
     updateUser:{url: config.basePath + '/domains/:domain_id/users', method: 'PUT', params: {user_id: '@user_id', username: '@username', password: '@password', role: '@role', enabled: '@enabled'}},
     deleteUser: { url: config.basePath + '/domains/test_users/:user_id', method: 'DELETE', params: {domain_key: '@domain_key', username: '@username'}},
