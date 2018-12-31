afd8600f0d9cc956083407c91a1a6993247d5ec7
brgemborys <bgemborys@qanairy.com>
2018-12-31 15:32:17 -0500

    Image Placeholder (#521)
    * Image Placeholder
    
    <div>
    																		<i class="browser_placeholder fa fa-image"></i>
    																		<div class="text-center">
    																			A test has not been run in this&nbsp;browser.
    																		</div>
    																	</div>
    
    * added in stand in image for empty screenshot for browsers
    
    * removed console log statements

--- app/components/discovery/controllers.js
+++ app/components/discovery/controllers.js
@@ -67,7 +67,6 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.DiscoveryService', 'Q
           .then(function(data){
             $scope.tests = data
             $scope.waitingOnTests = false;
-            console.log("$scope.tests :: "+data);
             if(data.length > 0){
               $scope.discoveredTestOnboardingEnabled = !$scope.hasUserAlreadyOnboarded('discovered-test');
               $scope.discoveredTestOnboardingIndex = 0;
@@ -92,7 +91,6 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.DiscoveryService', 'Q
 
         var channel = pusher.subscribe($scope.extractHostname($scope.current_domain.url));
         channel.bind('test-discovered', function(data) {
-          console.log("RECEIVED TEST FROM DISCOVERY");
           $scope.discoveredTestOnboardingEnabled = !$scope.hasUserAlreadyOnboarded('discovered-test');
           $scope.discoveredTestOnboardingIndex = 0;
           $scope.waitingOnTests = false;
@@ -101,13 +99,11 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.DiscoveryService', 'Q
         });
 
         channel.bind('path_object', function(data) {
-          console.log("received path object");
           $scope.discovery_status = JSON.parse(data);
           $scope.$apply();
         });
 
         channel.bind('discovery-status', function(data) {
-          console.log("discovery status");
           $scope.discovery_status = JSON.parse(data);
           $scope.$apply();
         });
@@ -575,7 +571,6 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.DiscoveryService', 'Q
         .then(function(data){
           $scope.tests = data
           $scope.waitingOnTests = false;
-          console.log("$scope.tests :: "+data);
           if(data.length > 0){
             $scope.discoveredTestOnboardingEnabled = !$scope.hasUserAlreadyOnboarded('discovered-test');
             $scope.discoveredTestOnboardingIndex = 0;
@@ -586,6 +581,15 @@ angular.module('Qanairy.discovery', ['ui.router', 'Qanairy.DiscoveryService', 'Q
           $scope.waitingOnTests = false;
         });
     });
+
+    $scope.doesScreenshotExistForBrowser = function(browser,  screenshots){
+      for(var idx=0; idx< screenshots.length; idx++){
+        if( screenshots[idx].browser === browser){
+          return  true;
+        }
+      }
+      return false;
+    }
 
     $rootScope.$on('missing_resorce_error', function (e){
       $scope.errors.push("We seem to have misplaced those records. Please try again. I'm sure we have them somewhere.");
