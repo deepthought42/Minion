8b863061ee131763311f967def3549e73209e180
Brandon Kindred <brandon.kindred@gmail.com>
2019-11-08 21:40:05 -0500

    element outline for path toggle

--- app/components/organisms/expandablePathToggle/expandablePathToggle.js
+++ app/components/organisms/expandablePathToggle/expandablePathToggle.js
@@ -45,8 +45,19 @@ angular.module('Qanairy.ExpandablePathToggle', [])
         return buildElementOutlineStyle(page, element);
       };
 
+
+
       function buildElementOutlineStyle(page, element){
-        var elem = angular.element(document.getElementById('expandable'+page.key));
+
+        var web_elem = document.getElementById('expandablePageState'+page.key);
+
+        var web_elem1 = document.getElementById('expandable'+page.key);
+        var elem = angular.element(web_elem);
+        console.log("element width :: "+elem.parent().width());
+        console.log("element height :: " +elem.parent().height());
+        console.log("page viewport height :: "+page.fullPageHeight);
+        console.log("page viewport width :: "+page.fullPageWidth);
+
         if($scope.width !== elem.width() && elem.width() !== 0){
           $scope.width = elem.width();
         }
@@ -54,20 +65,54 @@ angular.module('Qanairy.ExpandablePathToggle', [])
           $scope.height = elem.height();
         }
 
-        var scale_height = $scope.height/page.viewportHeight;
-        var scale_width = $scope.width/page.viewportWidth;
+        var marginRight = web_elem.offsetLeft;
+
+        //var marginRight = $("#expandablePageState"+page.key).css("margin-right");
+
+        var scale_height = $scope.height/page.fullPageHeight;
+        var scale_width = ($scope.width)/page.fullPageWidth;
 
         if(scale_height === 0){
           scale_height = scale_width;
         }
+        console.log("margin right ::  "+marginRight);
 
-        var element_width = element.width * scale_width;
+        console.log("scale width  ::   "+scale_width);
+        console.log("scale height  ::   "+scale_height);
+
+        var element_width = element.width * (scale_width*(($scope.width-(marginRight*2))/$scope.width);
         var element_height = element.height * scale_height;
 
-        var x_offset = (element.xlocation - page.scrollXOffset) * scale_width;
-        var y_offset = (element.ylocation - page.scrollYOffset) * scale_height;
+        console.log("element outline width ::  " +element_width);
+        console.log("element outline height :: "+element_height);
+        console.log("element xlocation  :: " +element.xlocation);
+        console.log("element ylocation :: " + element.ylocation);
+        var x_offset = Math.abs((element.xlocation )  * scale_width) + marginRight;
+        var y_offset = (element.ylocation) * scale_height;
+
+        console.log("element xlocation  :: " +x_offset);
+        console.log("element ylocation :: " + y_offset);
         var outline_style = "top: "+ y_offset +"px;left: "+ x_offset +"px; width: "+ element_width +"px; height:"+ element_height +"px";
         return outline_style;
+      }
+
+      /***
+       * get live runtime value of an element's css style
+       *   http://robertnyman.com/2006/04/24/get-the-rendered-style-of-an-element
+       *     note: "styleName" is in CSS form (i.e. 'font-size', not 'fontSize').
+       ***/
+      function getStyle(e, styleName) {
+          var styleValue = "";
+          if(document.defaultView && document.defaultView.getComputedStyle) {
+              styleValue = document.defaultView.getComputedStyle(e, "").getPropertyValue(styleName);
+          }
+          else if(e.currentStyle) {
+              styleName = styleName.replace(/\-(\w)/g, function (strMatch, p1) {
+                  return p1.toUpperCase();
+              });
+              styleValue = e.currentStyle[styleName];
+          }
+          return styleValue;
       }
     },
     link: function($scope, element) {
--- app/components/organisms/nonExpandablePathToggle/elementStateOutlineDirective.js
+++ app/components/organisms/nonExpandablePathToggle/elementStateOutlineDirective.js
@@ -26,26 +26,43 @@ angular.module('Qanairy.ElementStateOutline', [])
       };
 
       function buildElementOutlineStyle(page, element){
-        var elem = angular.element(document.getElementById('nonexpandable'+page.key));
+        var web_elem = document.getElementById('nonexpandable'+page.key);
+        var elem = angular.element(web_elem);
+        console.log("element width :: "+elem.width());
+        console.log("element height :: " +elem.height());
+        console.log("page viewport height :: "+page.fullPageHeight);
+        console.log("page viewport width :: "+page.fullPageWidth);
+
         if($scope.width !== elem.width() && elem.width() !== 0){
           $scope.width = elem.width();
         }
         if($scope.height !== elem.height() && elem.height() !== 0){
           $scope.height = elem.height();
         }
-
-        var scale_height = $scope.height/page.viewportHeight;
-        var scale_width = $scope.width/page.viewportWidth;
+        var scale_height = $scope.height/page.fullPageHeight;
+        var scale_width = ($scope.width-(marginRight*2))/page.fullPageWidth;
 
         if(scale_height === 0){
           scale_height = scale_width;
         }
 
+        var marginRight = getStyle(web_elem, 'margin-right');
+        console.log("margin right ::  "+marginRight);
+
+        console.log("scale width  ::   "+scale_width);
+        console.log("scale height  ::   "+scale_height);
+
         var element_width = element.width * scale_width;
         var element_height = element.height * scale_height;
 
-        var x_offset = (element.xlocation - page.scrollXOffset) * scale_width;
-        var y_offset = (element.ylocation - page.scrollYOffset) * scale_height;
+        console.log("element outline width ::  " +element_width);
+        console.log("element outline height :: "+element_height);
+
+        var x_offset = (element.xlocation - page.scrollXOffset) * scale_width - marginRight;
+        var y_offset = (element.ylocation - page.scrollYOffset) * scale_height - marginRight;
+
+        console.log("element xlocation  :: " +element.xlocation);
+        console.log("element ylocation :: " + element.ylocation);
         var outline_style = "top: "+ y_offset +"px;left: "+ x_offset +"px; width: "+ element_width +"px; height:"+ element_height +"px";
         return outline_style;
       }
