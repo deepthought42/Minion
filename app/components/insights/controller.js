4f856cc9d0144dc9de814f6435838fe681a0b0a5
Brandon Kindred <brandon.kindred@gmail.com>
2020-01-10 21:24:15 -0500

    added in details map to hold details values as string

--- app/components/insights/controller.js
+++ app/components/insights/controller.js
@@ -5,6 +5,9 @@ angular.module('Qanairy.insights', ['ui.router', 'Qanairy.PageService'])
 .config(['$stateProvider', function($stateProvider) {
   $stateProvider.state('main.insights', {
     url: "/insights",
+    params: {
+       page_key: null
+    },
     templateUrl: 'components/insights/index.html',
     controller: 'InsightCtrl',
     data: {
@@ -13,15 +16,18 @@ angular.module('Qanairy.insights', ['ui.router', 'Qanairy.PageService'])
   });
 }])
 
-.controller('InsightCtrl', ['$rootScope', '$scope', 'Page',  '$mdDialog', 'store', '$state',
-  function($rootScope, $scope, Page,  $mdDialog, store, $state) {
+.controller('InsightCtrl', ['$rootScope', '$scope', 'Page',  '$mdDialog', 'store', '$state', '$stateParams',
+  function($rootScope, $scope, Page,  $mdDialog, store, $state, $stateParams) {
     this._init = function(){
+      console.log("initializing insight page");
       $scope.errors = [];
       Page.getInsights({page_key: $stateParams.page_key}).$promise.
         then(function(data){
+          console.log("data returned");
           $scope.insights = data;
         })
         .catch(function(err){
+          console.log("error returned")
           $scope.errors.push(err);
         });
     };
--- app/components/page/controller.js
+++ app/components/page/controller.js
@@ -3,8 +3,8 @@
 angular.module('Qanairy.page', ['ui.router', 'Qanairy.DomainService', 'Qanairy.insights'])
 
 .config(['$stateProvider', function($stateProvider) {
-  $stateProvider.state('main.page', {
-    url: "/page",
+  $stateProvider.state('main.pages', {
+    url: "/pages",
     templateUrl: 'components/page/index.html',
     controller: 'PageCtrl',
     data: {
@@ -17,8 +17,8 @@ angular.module('Qanairy.page', ['ui.router', 'Qanairy.DomainService', 'Qanairy.i
   function($rootScope, $scope, Domain,  $mdDialog, store, $state) {
     this._init = function(){
       $scope.errors = [];
-      Domain.getPages({url: store.get("domain").url}).$promise.
-        then(function(data){
+      Domain.getPages({url: store.get("domain").url}).$promise
+        .then(function(data){
           $scope.pages = data;
         })
         .catch(function(err){
@@ -29,6 +29,10 @@ angular.module('Qanairy.page', ['ui.router', 'Qanairy.DomainService', 'Qanairy.i
     this._init();
 
     //navigate to page insights view and pass page_key as a stateParams variable
-    
+    $scope.showInsights = function(page_key){
+      $state.go('main.insights', {
+        page_key: page_key
+      });
+    }
   }
 ]);
